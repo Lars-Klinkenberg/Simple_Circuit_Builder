90ad7ed95f5606fe281544ef33f3bee9403d6ffa
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2025-02-24 01:26:34 +0100

    added function to render wires

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+
+export interface Wire {
+  start: Coordinate;
+  end: Coordinate;
+}
+export interface Coordinate {
+  row: number;
+  col: number;
+}
 
 @Component({
   selector: 'app-board',
@@ -9,6 +18,56 @@ import { Component, Input } from '@angular/core';
   styleUrl: './board.component.scss',
 })
 export class BoardComponent {
+  @ViewChild('board') boardContainer!: ElementRef;
+
   @Input() rowCount: number = 15;
   @Input() colCount: number = 40;
+
+  selectedHole: Coordinate | undefined = undefined;
+
+  wires: Wire[] = [];
+
+  holeSelected(row: number, col: number) {
+    let selection: Coordinate = { row, col };
+
+    if (!this.selectedHole || this.selectedHole == selection) {
+      this.selectedHole = selection;
+      return;
+    }
+    let wire = { start: this.selectedHole, end: selection };
+    this.wires.push(wire);
+    this.renderWire(wire);
+    this.selectedHole = undefined;
+  }
+
+  calcHoleIdString(row: number, col: number) {
+    return row + '-' + col;
+  }
+
+  renderWire(wire: Wire) {
+    if(wire.start.row > wire.end.row){
+      let temp = wire.start;
+      wire.start = wire.end;
+      wire.end = temp;
+    }
+
+    let hole1Container = document.getElementById(this.calcHoleIdString(wire.start.row, wire.start.col));
+    let hole2Container = document.getElementById(this.calcHoleIdString(wire.end.row, wire.end.col));
+
+    if (!hole1Container || !hole2Container) return;
+    let hole1Position = hole1Container.getBoundingClientRect();
+    let hole2Position = hole2Container.getBoundingClientRect();
+
+    let wireHeight =
+      Math.abs(hole1Position.top - hole2Position.top) + hole1Position.height;
+    let left = hole1Position.left;
+    let wireContainer = document.createElement('div');
+    wireContainer.style.position = 'absolute';
+    wireContainer.style.top = hole1Position.top.toString() + 'px';
+    wireContainer.style.left = left.toString() + 'px';
+    wireContainer.style.height = wireHeight.toString() + 'px';
+    wireContainer.classList.add('wire');
+
+    this.boardContainer.nativeElement.appendChild(wireContainer);
+  }
 }
