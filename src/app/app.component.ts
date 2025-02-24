c450c937d6a68cae10773a3ab14eb268b20971ea
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2025-02-25 00:01:16 +0100

    added option to change editType

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { BoardComponent } from './board/board.component';
 import { ColorSelectComponent } from './color-select/color-select.component';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
+export enum EditTypes {
+  'Wire',
+  'Component',
+  'Delete',
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BoardComponent, ColorSelectComponent],
+  imports: [CommonModule, BoardComponent, ColorSelectComponent, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'simple_circuit_builder';
+  editTypes = EditTypes;
+  currentEditType = this.editTypes.Wire;
   selectedColor: string = '#808080';
+
+  changeEditType(tool: EditTypes) {
+    this.currentEditType = tool;
+  }
 }
